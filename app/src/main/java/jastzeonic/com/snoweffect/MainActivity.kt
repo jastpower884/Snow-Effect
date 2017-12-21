cc4dd0eae26a88434fa0d3535211461e1968972d
Jast <jast.lai@yxunistar.com>
2017-12-21 11:01:37 +0800

    fixed untracked files

---
+++ app/src/main/java/jastzeonic/com/snoweffect/MainActivity.kt
@@ -0,0 +1,12 @@
+package jastzeonic.com.snoweffect
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
